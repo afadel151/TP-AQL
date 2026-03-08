645f6acb99226d531d4bafa039e8d52a44410f86
Akram Fadel <af8356207@gmail.com>
2026-03-08 10:27:44 +0100

    wrote concrete classes

---
+++ TP1/src/test/java/com/emp/AppTest.java
@@ -0,0 +1,13 @@
+package com.emp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void testExample() {
+        assertTrue(true);
+    }
+
+}
