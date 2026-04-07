247f58cce750fad341844433b4d1c4b59e98852a
Akram Fadel <af8356207@gmail.com>
2026-04-07 10:58:33 +0100

    make the service depend on interface

--- TP3/src/main/java/com/emp/services/UserService.java
+++ TP3/src/main/java/com/emp/services/UserService.java
@@ -1,18 +1,20 @@
 package com.emp.services;
 
-// import com.emp.models.User;
+import com.emp.shared.interfaces.UserRepositoryInterface;
+import com.emp.shared.models.User;
 // import com.emp;
-import com.emp.repositories.UserRepository;
+// import com.emp.repositories.UserRepository;
 
 public class UserService {
-     private UserRepository _userRepository;
-    public UserService(UserRepository userRepository)
+     private final UserRepositoryInterface _userRepository;
+
+    public UserService(UserRepositoryInterface userRepository)
     {
-        _userRepository = userRepository;
+        this._userRepository = userRepository;
     }
 
-    // public User getUserById(long id)
-    // {
-    //     return _userRepository.findUserById(id);
-    // }
+    public User getUserById(long id)
+    {
+        return _userRepository.findUserById(id);
+    }
 }
